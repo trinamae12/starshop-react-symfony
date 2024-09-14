03f91c369905c2087d5549e7ab92c67668c67353
trinamae12 <maetrina12@gmail.com>
2024-09-14 14:17:09 +0800

    Get all ships and update footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <div class="p-5 bg-white/5 mt-3 text-center">
-            Made with ❤️ by <a class="text-[#0086C4]" href="https://symfonycasts.com">SymfonyCasts</a>
+        <div className="p-5 bg-white/5 mt-3 text-center">
+            Made with ❤️ by <a className="text-[#0086C4]" href="https://symfonycasts.com">SymfonyCasts</a>
         </div>
     );
 }
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Sidebar from "./sidebar";
 
 const MainContent = () => {
+    const [ships, setShips] = useState([]);
+
+    useEffect(() => {
+        fetch('https://localhost/api/starships')
+        .then((response) => {
+            //console.log(response);
+            return response.json();
+        }).then((data) => setShips(data));
+    })
+
+    const getImage = (status) => {
+        if (status === 'in progress') {
+            return "/images/status-in-progress.png"
+        } else if (status === 'completed') {
+            return "/images/status-complete.png"
+        } else {
+            return "/images/status-waiting.png"
+        }
+    }
+
     return (
         <main className="flex flex-col lg:flex-row">
             <Sidebar />
@@ -13,36 +33,38 @@ const MainContent = () => {
 
             <div className="space-y-5">
                 {/* {{<!-- start ship item -->}} */}
-                    <div className="bg-[#16202A] rounded-2xl pl-5 py-5 pr-11 flex flex-col min-[1174px]:flex-row min-[1174px]:justify-between">
+                {ships.map((ship) => (
+                    <div key={ship.id} className="bg-[#16202A] rounded-2xl pl-5 py-5 pr-11 flex flex-col min-[1174px]:flex-row min-[1174px]:justify-between">
                         <div className="flex justify-center min-[1174px]:justify-start">
                             {/* <img className="h-[83px] w-[84px]" src="{{ asset(ship.statusImageFileName) }}" alt="Status: {{ ship.statusString }}" />*/}
-                            <img className="h-[83px] w-[84px]" src="/images/status-complete.png" alt="Status: Complete" />
+                            <img className="h-[83px] w-[84px]" src={getImage(ship.status)} alt="Status: Complete" />
                             <div className="ml-5">
                                 <div className="rounded-2xl py-1 px-3 flex justify-center w-32 items-center bg-amber-400/10">
                                     <div className="rounded-full h-2 w-2 bg-amber-400 blur-[1px] mr-2"></div>
-                                    <p className="uppercase text-xs text-nowrap">Ship Status</p>
+                                    <p className="uppercase text-xs text-nowrap">{ship.status}</p>
                                 </div>
                                 <h4 className="text-[22px] pt-1 font-semibold">
                                     <a
                                         className="hover:text-slate-200"
                                         // href="{{ path('app_starship_show', { id: ship.id }) }}"
                                         href="#"
-                                    >Ship Name</a>
+                                    >{ship.name}</a>
                                 </h4>
                             </div>
                         </div>
                         <div className="flex justify-center min-[1174px]:justify-start mt-2 min-[1174px]:mt-0 shrink-0">
                             <div className="border-r border-white/20 pr-8">
                                 <p className="text-slate-400 text-xs">Captain</p>
-                                <p className="text-xl">Ship Captain</p>
+                                <p className="text-xl">{ship.captain}</p>
                             </div>
 
                             <div className="pl-8 w-[100px]">
                                 <p className="text-slate-400 text-xs">Class</p>
-                                <p className="text-xl">Ship Class</p>
+                                <p className="text-xl">{ship.class}</p>
                             </div>
                         </div>
                     </div>
+                    ))}
                 {/** end ship item */}
             </div>
 
